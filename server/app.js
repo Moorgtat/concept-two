dd985fb574a91537ccf6a52ee4ade81059a73887
Moorgtat <mat.lecardinal@gmail.com>
2020-02-08 12:38:11 +0100

    Modif archi et structure server.

--- server/app.js
+++ server/app.js
@@ -3,17 +3,8 @@ const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-// app.use('/', indexRouter);
-// app.use('/status', indexRouter);
-// app.use('/register', indexRouter);
-// app.use('/users', usersRouter);
-
 const {sequelize} = require('./models');
 const config = require('./config/config');
-
 const app = express();
 
 app.use(morgan('combined'));
@@ -21,10 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-require('./routes')(app)
+require('./routes/routes')(app)
 
 sequelize.sync()
   .then(() => {
